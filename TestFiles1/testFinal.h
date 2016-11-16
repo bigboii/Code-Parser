8ed1ef11c1a4dcdefd888e95ecf4775274d2eff7
bigboii <yqkim91@gmail.com>
2016-11-16 15:50:29 -0500

    Add files via upload

---
+++ TestFiles1/testFinal.h
@@ -0,0 +1,21 @@
+#typedef TEST_TEST
+using Test = std::string;
+
+class TestClass() 
+{
+	
+}
+
+struct TestStruct() 
+{
+	
+}
+
+enum
+{
+	
+}
+
+typedef std::string token;
+
+using token = std::string;
